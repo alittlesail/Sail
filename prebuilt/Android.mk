4c208db1788e640b07f4eb26e4df79b93304cae3
xingfanou <757609414@qq.com>
2021-04-21 15:02:40 +0800

    添加mysql和openssl

---
+++ prebuilt/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := crypto
+LOCAL_SRC_FILES := libcrypto.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ssl
+LOCAL_SRC_FILES := libssl.a
+include $(PREBUILT_STATIC_LIBRARY)
