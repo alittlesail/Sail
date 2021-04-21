09a855e17f748965e5d24d922562d937af60ba0c
xingfanou <757609414@qq.com>
2021-04-21 15:18:37 +0800

    构建初始编译

--- Sail/SailClient/Main.cpp
+++ Sail/SailClient/Main.cpp
@@ -6,64 +6,51 @@
 #include <crtdbg.h>
 #endif // _WIN32
 
-#include <SDL_main.h>
+#include "sokol/sokol_app.h"
 
-#include "alittle_schedule.hpp"
+static void SokolInit()
+{
+}
 
-int main(int argc, char* argv[])
+static void SokolFrame()
+{
+    
+}
+
+static void SokolCleanUp()
+{
+    
+}
+
+static void SokolEvent(const sapp_event*)
+{
+}
+
+sapp_desc sokol_main(int argc, char* argv[])
 {
 #ifdef _WIN32
 #ifdef _DEBUG
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
-#endif // _WIN32	
-	return s_alittle_schedule.Run(argc, argv);
+#endif // _WIN32
+
+    sapp_desc desc = {0};
+    desc.width = 640;
+    desc.height = 480;
+    desc.init_cb = SokolInit;
+    desc.frame_cb = SokolFrame;
+    desc.cleanup_cb = SokolCleanUp;
+    desc.event_cb = SokolEvent;
+    return desc;
 }
 
-#define ALITTLE_AUDIO_IMPL
-#include "alittle_audio.hpp"
-#define ALITTLE_CSV_IMPL
-#include "alittle_csv.hpp"
-#define ALITTLE_DISPLAY_IMPL
-#include "alittle_display.hpp"
-#define ALITTLE_FONT_IMPL
-#include "alittle_font.hpp"
-#define ALITTLE_INPUT_IMPL
-#include "alittle_input.hpp"
-#define ALITTLE_NET_IMPL
-#include "alittle_net.hpp"
-#define ALITTLE_QUAD_IMPL
-#include "alittle_quad.hpp"
-#define ALITTLE_RENDER_IMPL
-#include "alittle_render.hpp"
-#define ALITTLE_SCHEDULE_IMPL
-#include "alittle_schedule.hpp"
-#define ALITTLE_SCRIPT_IMPL
-#include "alittle_script.hpp"
-#define ALITTLE_SYSTEM_IMPL
-#include "alittle_system.hpp"
-#define ALITTLE_TEXT_IMPL
-#include "alittle_text.hpp"
-#define ALITTLE_DISPLAYVIEW_IMPL
-#include "alittle_displayview.hpp"
-#define ALITTLE_TEXTURE_IMPL
-#include "alittle_texture.hpp"
+#define SOKOL_IMPL
+#define SOKOL_D3D11
+#include "sokol/sokol_app.h"
+
 #define CARP_CONSOLE_IMPL
 #include "Carp/carp_console.hpp"
 #define CARP_DUMP_IMPL
 #include "Carp/carp_dump.hpp"
 #define CARP_LOG_IMPL
 #include "Carp/carp_log.hpp"
-#define CARP_TASK_CONSUMER_IMPL
-#include "Carp/carp_task_consumer.hpp"
-#define CARP_SCHEDULE_IMPL
-#include "Carp/carp_schedule.hpp"
-#define CARP_SURFACE_IMPL
-#include "Carp/carp_surface_bind.hpp"
-#define CARP_MIXER_IMPL
-#include "Carp/carp_mixer.hpp"
-
-#include "Carp/carp_lua_debug.hpp"
-CarpLuaDebugServer s_alittle_lua_debug_server;
-#include "Carp/carp_lua_profiler.hpp"
-CarpLuaProfiler s_alittle_lua_profiler;
